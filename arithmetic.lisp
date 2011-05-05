5f67d36b1998f2c6d4d469bb74602c8de07d10fe
Olaf Merkert <olaf@m-merkert.de>
2011-05-05 07:01:55 +0200

    Cumulative Sums

--- arithmetic.lisp
+++ arithmetic.lisp
@@ -1,6 +1,7 @@
 (in-package #:ol-utils)
 
-(export '(sum prod))
+(export '(sum prod
+          cumulative-sums))
 
 ;; TODO capture the abstraction
 (defmacro! sum ((var start &optional end (step 1)) expr)
@@ -12,3 +13,10 @@
   `(do-range (,var ,start ,end ,step ,g!prod)
        ((,g!prod 1))
      (setf ,g!prod (* ,g!prod ,expr))))
+
+(defun cumulative-sums (seq &optional (start 0))
+  "Calculate the cumulative sums of seq."
+  (dotimes (i (length seq))
+    (setf (elt seq i)
+          (incf start (elt seq i))))
+  seq)
