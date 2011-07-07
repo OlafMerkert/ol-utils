c7dfce9935ef4a717c9329e7ded1eaef372a149a
Olaf Merkert <olaf@m-merkert.de>
2011-07-07 22:42:07 +0200

    using lisp-syntax when definining new classes

--- qt/camelcase.lisp
+++ qt/camelcase.lisp
@@ -44,3 +44,20 @@
 (defun make-qapp ()
   (setf qt-user:*application*
         (make-qapplication)))
+
+(defgeneric translate-options (key options))
+
+(defmethod translate-options (key options)
+  (cons key options))
+
+(defmethod translate-options ((key (eql :override)) method-names)
+  `(,key ,(mapcar #`(,(lisp->camel (symbol-name a1)) ,a1) method-names)))
+
+(defmacro defqtclass (name direct-superclasses direct-slots &rest options)
+  (let ((lisp-superclasses (remove-if-not #'symbolp direct-superclasses))
+        (qt-superclasses   (remove-if-not #'stringp direct-superclasses)))
+    `(defclass ,name ,lisp-superclasses
+       (,@direct-slots)
+       (:metaclass qt:qt-class)
+       (:qt-superclass ,@qt-superclasses)
+       ,@(mapcar (lambda (o) (translate-options (car o) (cdr o))) options))))
