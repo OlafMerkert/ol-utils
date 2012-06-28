ebd9819731c1dd37d4041e951fb54d50ee0716a7
Olaf Merkert <olaf@m-merkert.de>
2012-06-28 21:30:52 +0200

    map-tree and map-tree-if

--- trees.lisp
+++ trees.lisp
@@ -1,6 +1,8 @@
 (in-package #:ol-utils)
 
-(export '(matching-tree-nodes))
+(export '(matching-tree-nodes
+          map-tree
+          map-tree-if))
 
 (defun matching-tree-nodes (pred tree)
   "Give a list of all nodes (and leafs) that satisfy the predicate."
@@ -10,3 +12,18 @@
                   (list tree))
              (matching-tree-nodes pred (cdr tree)))
       #1#))
+
+(defun map-tree (fn tree)
+  "Map FN on all leafs of TREE, conserving the structure."
+  (if (atom tree)
+      (funcall fn tree)
+      (cons (map-tree fn (car tree))
+            (map-tree fn (cdr tree)))))
+
+(defun map-tree-if (pred fn tree)
+  "Map FN just on the leafs of TREE, that satisfy PRED."
+  (map-tree (lambda (leaf)
+              (if (funcall pred leaf)
+                  (funcall fn leaf)
+                  leaf))
+            tree))
