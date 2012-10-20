1da98476930fc9c665aa4d62a9eb452b3b29f4bc
Olaf Merkert <olaf@m-merkert.de>
2012-10-20 11:29:22 +0200

    use generic function instead of a hash-table of functions for the app starter

--- applications.lisp
+++ applications.lisp
@@ -1,22 +1,16 @@
 (in-package :ol-utils)
 
-(defvar applications (make-hash-table))
-
-(defun start-app (id)
-  "Start a registered (and installed) cl application."
-  (aif (gethash id applications)
-       (funcall it)
-       (format t "Unknown application ~A~%" id)))
+(defgeneric start-app (id)
+  (:documentation "Start a registered (and installed) cl application."))
 
 (defmacro define-application (id system package function)
   "Define a starter function for ID that loads a SYSTEM, then calls
 FUNCTION in PACKAGE. ID ought to be a keyword symbol, on the other
 arguments we call mkstr, so use symbols or strings as you prefer."
-  `(setf (gethash ,id applications)
-         (lambda ()
-           (ql:quickload ,(mkstr system))
-           (funcall
-            (intern ,(mkstr function)
-                    ,(mkstr package))))))
+  `(defmethod start-app ((id (eql ,id)))
+     (ql:quickload ,(mkstr system))
+     (funcall
+      (intern ,(mkstr function)
+              ,(mkstr package)))))
 
 (define-application :tvs tv-series-status tvs-clim tv-series-display)
