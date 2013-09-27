0485a834ccc753fc1dad9ac78cbc799067335d16
Olaf Merkert <olaf@m-merkert.de>
2013-09-27 08:08:05 +0200

    start-app can now list available applications

--- applications.lisp
+++ applications.lisp
@@ -7,12 +7,20 @@
   "Define a starter function for ID that loads a SYSTEM, then calls
 FUNCTION in PACKAGE. ID ought to be a keyword symbol, on the other
 arguments we call mkstr, so use symbols or strings as you prefer."
-  `(defmethod start-app ((id (eql ,id)) &rest args)
-     (ql:quickload ,(mkstr system))
-     (apply
-      (intern ,(mkstr function)
-              ,(mkstr package))
-      args)))
+  `(progn
+     (pushnew ,id application-registry)
+     (defmethod start-app ((id (eql ,id)) &rest args)
+      (ql:quickload ,(mkstr system))
+      (apply
+       (intern ,(mkstr function)
+               ,(mkstr package))
+       args))))
+
+(defvar application-registry (list :list))
+
+(defmethod start-app ((id (eql :list)) &rest args)
+  (declare (ignore args))
+  application-registry)
 
 (define-application :tvs tv-series-status tvs-clim tv-series-display)
 
@@ -21,3 +29,5 @@ arguments we call mkstr, so use symbols or strings as you prefer."
 (define-application :mi math-interactor math-interactor math-interactor)
 
 (define-application :cpi complex-plane-interactor complex-plane-geometry complex-plane-interactor)
+
+(define-application :imdbg download-helpers download-imdb-gallery download-gallery-interactive)
